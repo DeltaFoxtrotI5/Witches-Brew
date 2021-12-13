d484c57931fd97b0f7cf03ddb9229239428d2f80
MUKILTEO\1709518 <1709518@mukilteo.wednet.edu>
2021-12-13 13:36:07 -0800

    credits scene worked on

---
+++ Assets/Buddy/ProgectileBehavior.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgectileBehavior : MonoBehaviour
+{
+    public float Speed = 4.5f;
+
+    private void Update()
+    {
+        transform.position += -transform.right * Time.deltaTime * Speed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
+
+}
---
+++ Assets/DoNotTouch(MainScene)/Credits/CreditRoller.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditRoller : MonoBehaviour
+{
+
+    private static int nScreens = 3;
+    private GameObject[] creditScenes = new GameObject[nScreens];
+    private static int swapCount = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //for each credit scene, add a refrence here
+        creditScenes[0] = GameObject.Find("Credit1");
+        creditScenes[1] = GameObject.Find("Credit2");
+        creditScenes[2] = GameObject.Find("Credit3");
+        creditScenes[3] = GameObject.Find("Credit4");
+        creditScenes[4] = GameObject.Find("Credit5");
+        creditScenes[5] = GameObject.Find("Credit6");
+
+        //turn all scenes off
+        for (int i = 0; i <nScreens; i++)
+        {
+            creditScenes[i].SetActive(false);
+        }
+        //turn back on the "0th"
+        creditScenes[0].SetActive(true);
+
+    }
+
+    public void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            int CurrentScene = swapCount % nScreens;
+            creditScenes[CurrentScene].SetActive(false);
+            swapCount++;
+            CurrentScene = swapCount % nScreens;
+            creditScenes[CurrentScene].SetActive(true);
+            Debug.Log(CurrentScene);
+        }
+
+
+    }
+}
