be4da2d2bd675b0afcd7aea7f7688bffd4a0d96b
MUKILTEO\1710220 <griffindbarlow@gmail.com>
2022-01-18 13:36:40 -0800

    Kill

---
+++ Assets/DoNotTouch(MainScene)/MainScene/Spikekill.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spikekill : MonoBehaviour
+{
+    public Animator ani;
+    private GameObject me;
+
+      if (collision.gameObject.tag == "Player") 
+        {
+         ani.Play("DestroyC");
+            //Debug.Log(collision.gameObject.tag + "====================================================================");
+            me = collision.gameObject;
+            // Destroy(me);
+            Invoke("kill", 2f);
+}
+        
+           
+     
+
+
+}
